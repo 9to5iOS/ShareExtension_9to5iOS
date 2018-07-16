024ca569996d8ab949a20ef5be53bc410848a760
Deepak singh rawat <deepak.rawat@spicedigital.in>
2018-07-16 12:34:25 +0530

    Initial Commit

---
+++ ShareExtension_9to5iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShareExtension_9to5iOS
+//
+//  Created by admin on 16/07/18.
+//  Copyright © 2018 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
